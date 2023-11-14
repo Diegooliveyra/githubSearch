ed1a791a72a0e11b34ff5dd8bed48393520fb01f
Diego Oliveira <diegooliveirabryto@gmail.com>
2023-11-14 17:22:13 -0300

    feat: init user page

--- src/App.tsx
+++ src/App.tsx
@@ -5,12 +5,18 @@ import Home from './pages/Home';
 import { ThemeProvider } from 'styled-components';
 import theme from './styles/theme';
 import GlobalStyles from './styles/GlobalStyles';
+import User from './pages/User';
 
 function App() {
   const router = createBrowserRouter([
     {
       path: '/',
       element: <Home />,
+    },
+
+    {
+      path: 'user/:id',
+      element: <User />,
     },
   ]);
   return (
