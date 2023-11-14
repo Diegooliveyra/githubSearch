32f4a3a6f00a7c65268ca3a8914dbbee1d2da404
Diego Oliveira <diegooliveirabryto@gmail.com>
2023-11-14 17:21:07 -0300

    feat: add render options

--- src/utils/tests/helpers.tsx
+++ src/utils/tests/helpers.tsx
@@ -1,6 +1,9 @@
 import { ThemeProvider } from 'styled-components';
-import { render, RenderResult } from '@testing-library/react';
+import { render, RenderOptions, RenderResult } from '@testing-library/react';
 import theme from '@/styles/theme';
 
-export const renderWithTheme = (children: React.ReactNode): RenderResult =>
-  render(<ThemeProvider theme={theme}>{children}</ThemeProvider>);
+export const renderWithTheme = (
+  children: React.ReactNode,
+  options?: RenderOptions
+): RenderResult =>
+  render(<ThemeProvider theme={theme}>{children}</ThemeProvider>, options);
