aaef2ca3b2ca6c5087dd54fe95c221cecd347edf
Diego Oliveira <diegooliveirabryto@gmail.com>
2023-11-14 17:20:15 -0300

    feat: refatore header

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,13 +1,19 @@
-import { ReactSVG } from 'react-svg';
-
 import * as S from './styles';
+import logo from '../../assets/imgs/logo-x.svg';
+import backIcon from '../../assets/icons/ic-back.svg';
+import { ReactSVG } from 'react-svg';
+import { useNavigate } from 'react-router-dom';
 
 const Header = () => {
+  const navigate = useNavigate();
   return (
     <S.Header>
       <S.HeaderContent>
-        <ReactSVG src="src/assets/imgs/logo-x.svg" />
-        <ReactSVG src="src/assets/icons/ic-back.svg" />
+        <ReactSVG src={logo} role="figure" />
+
+        <S.IconButton onClick={() => navigate('/')} title="Voltar">
+          <ReactSVG src={backIcon} role="img" />
+        </S.IconButton>
       </S.HeaderContent>
     </S.Header>
   );
--- src/components/Header/styles.tsx
+++ src/components/Header/styles.tsx
@@ -1,18 +1,29 @@
 import styled, { css } from 'styled-components';
 
-export const Header = styled.main`
+export const Header = styled.header`
   ${({ theme }) => css`
     width: 100%;
     padding: ${theme.spacings.xsmall};
   `}
 `;
 
-export const HeaderContent = styled.main`
+export const HeaderContent = styled.div`
   ${({ theme }) => css`
     display: flex;
     align-items: center;
     justify-content: space-between;
     max-width: ${theme.grid.container};
     margin: 0 auto;
+  `}
+`;
+
+export const IconButton = styled.button`
+  ${() => css`
+    display: flex;
+    align-items: center;
+    justify-content: center;
+    background-color: transparent;
+    border: none;
+    cursor: pointer;
   `}
 `;
