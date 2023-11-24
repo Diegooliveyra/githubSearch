4de8ff133a72e4515e7278ab56bf8ba9555b3778
Diego Oliveira <diegooliveirabryto@gmail.com>
2023-11-23 21:41:25 -0300

    chore: config service alias

--- vite.config.ts
+++ vite.config.ts
@@ -13,6 +13,7 @@ export default defineConfig({
       '@components': path.resolve(__dirname, './src/components'),
       '@utils': path.resolve(__dirname, './src/utils'),
       '@styles': path.resolve(__dirname, './src/styles'),
+      '@services': path.resolve(__dirname, './src/services'),
     },
   },
 });
